181c0eff9549be6a67e8b957d5c18cdec4b0ad3a
Vince Lehman <vslehman@memphis.edu>
2016-03-21 13:25:11 -0700

    Run JSON API to access real-time NFD stats
    Change-Id: Iab0302fcc1c77e0d6b6ad4311efcdcb0d8c4525d

--- install.sh
+++ install.sh
@@ -216,6 +216,7 @@ function minindn {
     sudo cp ndn_utils/topologies/default-topology.conf "$install_dir"
     sudo cp ndn_utils/topologies/minindn.testbed.conf "$install_dir"
     sudo cp -r nfd_status_fetcher/ "$install_dir/status/"
+    sudo cp -r rt-stats-server/ "$install_dir/rt-stats-server/"
     sudo python setup.py install
 }
 
---
+++ rt-stats-server/rt-server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import json
+from bottle import *
+from bottle import PasteServer
+
+class RtStats:
+    def __init__(self, name):
+        self.stats = {}
+        self.stats['name'] = name
+        self.stats['uptime'] = 0
+        self.stats['nNameTreeEntries'] = 0
+        self.stats['nFibEntries'] = 0
+        self.stats['nPitEntries'] = 0
+        self.stats['nMeasurementsEntries'] = 0
+        self.stats['nCsEntries'] = 0
+        self.stats['nInInterests'] = 0
+        self.stats['nOutInterests'] = 0
+        self.stats['nInDatas'] = 0
+        self.stats['nOutDatas'] = 0
+
+    def processLine(self, line):
+        line = line.strip().rstrip()
+        tokens = line.split('=')
+        key = tokens[0]
+        value = tokens[1]
+
+        self.stats[key] = value
+
+@get('/')
+def serve_json():
+    statsDir = '/tmp/stats/status/rt/'
+    statsJson = []
+
+    for filename in os.listdir(statsDir):
+        nodeName = filename.strip('.txt')
+
+        stats = RtStats(nodeName)
+
+        with open('{}{}'.format(statsDir, filename), 'r') as inFile:
+           for line in inFile:
+                stats.processLine(line)
+
+        statsJson.append(stats.stats)
+
+    return json.dumps(statsJson)
+
+run(host='127.0.0.1', port=9000, server=PasteServer)
